01512e40300bdc6ab6fb01e79b1adf33ee13e910
hqnghi88 <hqnghi88@gmail.com>
2021-12-02 01:20:53 +0700

    test

--- config/passport.js
+++ config/passport.js
@@ -7,11 +7,11 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-const local = require('./passport/local');
-const google = require('./passport/google');
-const twitter = require('./passport/twitter');
-const linkedin = require('./passport/linkedin');
-const github = require('./passport/github');
+// const local = require('./passport/local');
+// const google = require('./passport/google');
+// const twitter = require('./passport/twitter');
+// const linkedin = require('./passport/linkedin');
+// const github = require('./passport/github');
 
 /**
  * Expose
