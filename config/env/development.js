37841f065c5a2bbec8603905f250f84438425a93
hqnghi88 <hqnghi88@gmail.com>
2021-12-02 01:07:59 +0700

    test

--- config/env/development.js
+++ config/env/development.js
@@ -12,20 +12,21 @@ module.exports = {
     clientID: process.env.TWITTER_CLIENTID,
     clientSecret: process.env.TWITTER_SECRET,
     callbackURL: `http://localhost:${port}/auth/twitter/callback/`
-  },
-  github: {
-    clientID: process.env.GITHUB_CLIENTID,
-    clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: `http://localhost:${port}/auth/github/callback`
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_CLIENTID,
-    clientSecret: process.env.LINKEDIN_SECRET,
-    callbackURL: `http://localhost:${port}/auth/linkedin/callback`
-  },
-  google: {
-    clientID: process.env.GOOGLE_CLIENTID,
-    clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: `http://localhost:${port}/auth/google/callback`
   }
+  // ,
+  // github: {
+  //   clientID: process.env.GITHUB_CLIENTID,
+  //   clientSecret: process.env.GITHUB_SECRET,
+  //   callbackURL: `http://localhost:${port}/auth/github/callback`
+  // },
+  // linkedin: {
+  //   clientID: process.env.LINKEDIN_CLIENTID,
+  //   clientSecret: process.env.LINKEDIN_SECRET,
+  //   callbackURL: `http://localhost:${port}/auth/linkedin/callback`
+  // },
+  // google: {
+  //   clientID: process.env.GOOGLE_CLIENTID,
+  //   clientSecret: process.env.GOOGLE_SECRET,
+  //   callbackURL: `http://localhost:${port}/auth/google/callback`
+  // }
 };
